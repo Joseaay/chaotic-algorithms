edb76f7b19bc2a7d1b93d0faac7e10d184d5e744
Jose Ant. Ayllon <joseayllonll@hotmail.com>
2019-04-04 19:57:04 +0100

    - Improve code and structure

--- js/algorithms-js/feigenbaum.js
+++ js/algorithms-js/feigenbaum.js
@@ -1,25 +1,25 @@
-(function () {
+(function() {
     var c = document.getElementById("c"),
         ctx = c.getContext("2d"),
         w = c.width,
         h = c.height,
         st = 2 / w,
-        fx = function (x1, r) {
+        fx = function(x1, r) {
             return r * x1 * (1 - x1);
         },
-        it = function (r) {
+        it = function(r) {
             var idx = 0,
                 x = 0.5,
                 xc = w * ((r - 2) / 2);
             while (idx++ < 2000) {
                 x = fx(x, r);
-                ctx.fillRect(xc, h - (x * h), 1, 1);
+                ctx.fillRect(xc, h - x * h, 1, 1);
             }
         };
     ctx.fillStyle = "rgba(30,0,0,0.05)";
     for (r = 2; r < 4; r += st) {
-        (function (r1) {
-            setTimeout(function () {
+        (function(r1) {
+            setTimeout(function() {
                 it(r1);
             }, 0);
         })(r);
--- js/algorithms-js/mandelbrot-julia.js
+++ js/algorithms-js/mandelbrot-julia.js
@@ -1,12 +1,6 @@
-// The function gets called when the window is fully loaded
-
 var r = 0;
 var g = 0;
-var b = 2;
-var blue_slider = document.getElementsByName("blue");
-var red_slider = document.getElementsByName("red");
-var green_slider = document.getElementsByName("green");
-var ck = "mousedown";
+var b = 0;
 
 function mandelbrot() {
     var canvas = document.getElementById("viewport");
@@ -20,24 +14,23 @@ function mandelbrot() {
     var imagedata = context.createImageData(imagew, imageh);
 
     // Parametros para el zoom
-    var osx = -imagew/2;
-    var osy = -imageh/2;
+    var osx = -imagew / 2;
+    var osy = -imageh / 2;
     var panx = -110;
     var pany = 0;
     var zoom = 205;
     var paleta = [];
 
     // Máximo númerod e iteraciones por pixel
-    var iteracionesmaximas = parseInt(document.getElementsByName("iterations")[0].value);
+    var iteracionesmaximas = parseInt(
+        document.getElementsByName("iterations")[0].value
+    );
 
     function init() {
+        canvas.addEventListener("dblclick", onMouseDown);
+        canvas.addEventListener("mousedown", unClick);
 
-        canvas.addEventListener(ck, onMouseDown);
-        blue_slider[0].addEventListener("input", updateColor);
-        green_slider[0].addEventListener("input", updateColor);
-        red_slider[0].addEventListener("input", updateColor);
-
-        // Generat paleta
+        // Genera paleta
         generatepaleta();
 
         // Genera imagen
@@ -46,7 +39,6 @@ function mandelbrot() {
         main(0);
     }
 
-
     function main(tframe) {
         window.requestAnimationFrame(main);
 
@@ -54,29 +46,21 @@ function mandelbrot() {
         context.putImageData(imagedata, 0, 0);
     }
 
-    function updateColor(){
-      r = parseInt(blue_slider[0].value);
-      g = parseInt(green_slider[0].value);
-      b = parseInt(red_slider[0].value);
-      generatepaleta()
-      generateImage();
-    }
-
     // Genera paleta
     function generatepaleta() {
-      // Calcula el gradiente
+        // Calcula el gradiente
         var roffset = r;
         var goffset = g;
         var boffset = b;
 
-        for (var i=0; i<256; i++) {
-            paleta[i] = { r:roffset, g:goffset, b:boffset};
+        for (var i = 0; i < 256; i++) {
+            paleta[i] = { r: roffset, g: goffset, b: boffset };
 
             if (i < 64) {
                 roffset += 3;
-            } else if (i<128) {
+            } else if (i < 128) {
                 goffset += 3;
-            } else if (i<192) {
+            } else if (i < 192) {
                 boffset += 3;
             }
         }
@@ -84,8 +68,8 @@ function mandelbrot() {
 
     // Genera la imagen del fractal
     function generateImage() {
-        for (var y=0; y<imageh; y++) {
-            for (var x=0; x<imagew; x++) {
+        for (var y = 0; y < imageh; y++) {
+            for (var x = 0; x < imagew; x++) {
                 iterate(x, y, iteracionesmaximas);
             }
         }
@@ -102,7 +86,7 @@ function mandelbrot() {
         var ry = 0;
 
         var iterations = 0;
-        while (iterations < iteracionesmaximas && (rx * rx + ry * ry <= 4)) {
+        while (iterations < iteracionesmaximas && rx * rx + ry * ry <= 4) {
             rx = a * a - b * b + x0;
             ry = 2 * a * b + y0;
 
@@ -111,30 +95,31 @@ function mandelbrot() {
             iterations++;
         }
 
-
         // Obtener la paleta de color conforme al numero de iteraciones
         var color;
         if (iterations == iteracionesmaximas) {
-            color = { r:0, g:0, b:0}; // Black
+            color = { r: 0, g: 0, b: 0 };
         } else {
-            var index = Math.floor((iterations / (iteracionesmaximas-1)) * 255);
+            var index = Math.floor(
+                (iterations / (iteracionesmaximas - 1)) * 255
+            );
             color = paleta[index];
         }
 
         // Aplica color
         var pixelindex = (y * imagew + x) * 4;
         imagedata.data[pixelindex] = color.r;
-        imagedata.data[pixelindex+1] = color.g;
-        imagedata.data[pixelindex+2] = color.b;
-        imagedata.data[pixelindex+3] = 255;
+        imagedata.data[pixelindex + 1] = color.g;
+        imagedata.data[pixelindex + 2] = color.b;
+        imagedata.data[pixelindex + 3] = 255;
     }
 
     // Zoom
     function fzoom(x, y, f, zoomin) {
         if (!zoomin) {
-          zoom /= f;
-          panx = (x + osx + panx) / f;
-          pany = (y + osy + pany) / f;
+            zoom /= f;
+            panx = (x + osx + panx) / f;
+            pany = (y + osy + pany) / f;
         } else {
             zoom *= f;
             panx = f * (x + osx + panx);
@@ -160,33 +145,41 @@ function mandelbrot() {
         generateImage();
     }
 
+    function unClick(e) {
+        var pos = getMousePos(canvas, e);
+
+        var x0 = (pos.x + osx + panx) / zoom;
+        var y0 = (pos.y + osy + pany) / zoom;
+
+        document.getElementsByName("c1")[0].value = x0;
+        document.getElementsByName("c2")[0].value = y0;
+        actualizar();
+    }
+
     // Consigue la posición del ratón
     function getMousePos(canvas, e) {
         var rect = canvas.getBoundingClientRect();
         return {
-            x: Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width),
-            y: Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height)
+            x: Math.round(
+                ((e.clientX - rect.left) / (rect.right - rect.left)) *
+                    canvas.width
+            ),
+            y: Math.round(
+                ((e.clientY - rect.top) / (rect.bottom - rect.top)) *
+                    canvas.height
+            )
         };
     }
 
     init();
-};
-
-function reset(){
-  document.getElementsByName("blue")[0].value = 2;
-  document.getElementsByName("red")[0].value = 0;
-  document.getElementsByName("green")[0].value = 0;
-  document.getElementsByName("iterations")[0].value = 200;
-  r = parseInt(blue_slider[0].value);
-  g = parseInt(green_slider[0].value);
-  b = parseInt(red_slider[0].value);
-  changeColor();
-  mandelbrot();
 }
 
-function changeColor(){
-  document.getElementsByName("blue_label")[0].innerHTML = blue_slider[0].value;
-  document.getElementsByName("red_label")[0].innerHTML = red_slider[0].value;
-  document.getElementsByName("green_label")[0].innerHTML = green_slider[0].value;
+function reset() {
+    document.getElementsByName("blue")[0].value = 2;
+    document.getElementsByName("red")[0].value = 0;
+    document.getElementsByName("green")[0].value = 0;
+    document.getElementsByName("iterations")[0].value = 200;
 
+    changeColor();
+    mandelbrot();
 }
